0882e3de8ceb3abae53b85aa95918a63b589faf2
Ramiro Quiñoneros <102091274+Ramiroaya@users.noreply.github.com>
2022-04-25 19:24:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ let rotulo2 = <HTMLParagraphElement>document.getElementById("rotulo2");
 let rotulo3 = <HTMLParagraphElement>document.getElementById("rotulo3");
 let dato1 = <HTMLInputElement>document.getElementById("dato1");
 let dato2 = <HTMLInputElement>document.getElementById("dato2");
-
+  function potenciar (base: number,exponente: number): number {
+    return Math.pow(base,exponente);
+  };
 
 rotulo1.innerHTML = "Ingrese la base";
 rotulo2.innerHTML = "Ingrese el Exponente";
@@ -18,15 +20,12 @@ btnEnviar2.addEventListener("click", () => {
   let exponente = Number(dato2.value);
   
 
- btnEnviar3.addEventListener("Click", () => {
+ btnEnviar3.addEventListener("click", () => {
    if(exponente===0){
       console.log("El resultado es:1"); 
       }else{ 
           let resultado = Number(potenciar(base,exponente));
           console.log("El resultado es:" + resultado);  
-  function potenciar (base: number,exponente: number): number {
-    return Math.pow(base,exponente);
-  };
   };
   });
 };
